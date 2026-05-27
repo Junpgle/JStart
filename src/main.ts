99a3c7e08bae4b40fbc908d6451033bf7227a90d
Junpgle . <81464408+Junpgle@users.noreply.github.com>
2026-05-27 20:57:32 +0800

    美化番茄钟计时UI

--- src/main.ts
+++ src/main.ts
@@ -517,8 +517,25 @@ function updatePomodoroDisplay(): void {
   const min = Math.floor(display / 60)
   const sec = display % 60
   const title = pomodoroTodoTitle ? `<div class="clock-sub">${pomodoroTodoTitle}</div>` : ''
-  const icon = `<span class="pomodoro-icon"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-3 12H7v-2h10v2zm0-3H7V9h10v2zm0-3H7V6h10v2z"/></svg></span>`
-  clock.innerHTML = `${icon}${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}${title}`
+  const badge = `<span class="pomodoro-badge">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 21C16.9706 21 21 17.4183 21 13C21 8.58172 16.9706 5 12 5C7.02944 5 3 8.58172 3 13C3 17.4183 7.02944 21 12 21Z" fill="url(#tomatoGrad)"/>
+      <path d="M7.5 10C6.5 11 6 12.5 6 13.5C6 14 6.2 14.2 6.5 14C7 13.5 8 11.5 8 10.5C8 10.1 7.8 9.7 7.5 10Z" fill="white" fill-opacity="0.5"/>
+      <path d="M12 5V2.5C12 2.22386 12.2239 2 12.5 2C12.7761 2 13 2.22386 13 2.5V5H12Z" fill="#2ECC71"/>
+      <path d="M12 5.5C10 4.5 7.5 4.5 6.5 5C8 6 10.5 6 12 5.5Z" fill="#27AE60"/>
+      <path d="M12 5.5C14 4.5 16.5 4.5 17.5 5C16 6 13.5 6 12 5.5Z" fill="#27AE60"/>
+      <path d="M12 5.5C12 3.5 11 1.5 9.5 1C10.5 2.5 11.5 4 12 5.5Z" fill="#219A52"/>
+      <path d="M12 5.5C12 3.5 13 1.5 14.5 1C13.5 2.5 12.5 4 12 5.5Z" fill="#219A52"/>
+      <defs>
+        <radialGradient id="tomatoGrad" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(10 10) rotate(45) scale(12 12)">
+          <stop offset="0%" stop-color="#FF6B6B"/>
+          <stop offset="60%" stop-color="#FF4757"/>
+          <stop offset="100%" stop-color="#D63031"/>
+        </radialGradient>
+      </defs>
+    </svg>
+  </span>`
+  clock.innerHTML = `${badge}<span class="pomodoro-time">${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}</span>${title}`
   clock.className = 'clock pomodoro'
 }
 
